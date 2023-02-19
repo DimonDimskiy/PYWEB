473a4aea5ba4c97dcb954235fb858451de373c15
DimonDimskiy <skdmyr@gmail.com>
2023-02-19 11:14:36 +0100

    Third practice finished

---
+++ apps/checkout/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views import View
+
+
+class ViewCheckout(View):
+    def get(self, request):
+        return render(request, 'checkout/checkout.html')
+
+# Create your views here.
